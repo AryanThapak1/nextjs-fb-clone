5d1d795b4874e319067258eb4ce0e1dd37d577ad
AryanThapak1 <109138686+AryanThapak1@users.noreply.github.com>
2023-08-12 23:00:40 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,7 +5,7 @@ export default async function handler(req,res){
   if(req.method==='POST')
   {
      const data=req.body;
-     const client=await MongoClient.connect('mongodb+srv://aryanthapak8:367IVGvvJCun7WDu@cluster0.8s3by7c.mongodb.net/?retryWrites=true&w=majority')
+     const client=await MongoClient.connect("put your mongo client here")
      const db=client.db();
      const credentials=db.collection('credentials');
      const result=await credentials.insertOne(data);
